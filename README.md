fe91d4b70bd205670bd71a785385f6b0ed8da872
David Currie <david_currie@uk.ibm.com>
2014-05-01 13:25:30 +0100

    Initial version

--- README.md
+++ README.md
@@ -1,4 +1,65 @@
-ci.gradle
-=========
+# ci.gradle
 
-Gradle plugins for managing Liberty profile servers #devops
+A Gradle plugin to support the manipulation of WebSphere Application Server Liberty Profile servers.
+
+## Build
+
+Clone this repository and then, with a JRE on the path, execute the following command in the root directory.
+
+```bash
+gradlew build
+```
+
+This will download Gradle and then build the plugin `liberty-gradle-plugin-1.0.jar` in to the `build\libs` directory. It is also possible to install the plugin in to your local Maven repository using `gradlew install`.
+
+## Usage
+
+Within your Gradle build script, you need to set up the classpath to include the Liberty Gradle plugin and the `ws-server.jar` from a WebSphere Application Server Liberty Profile installation. If you have installed the plugin in to your local Maven repository, this would be achieved as follows:
+
+```groovy
+buildscript {
+    repositories {
+        mavenLocal()
+    }	
+    dependencies {
+        classpath 'com.ibm.websphere.wlp.gradle.plugins:liberty-gradle-plugin:1.0'
+        classpath files('c:/wlp/bin/tools/ws-server.jar')
+    }
+}
+```
+
+Alternatively, you might choose to include the plugin JAR file as part of your build project. For example:
+
+```groovy
+buildscript {
+    dependencies {
+        classpath files('gradle/liberty-gradle-plugin.jar')
+        classpath files('c:/wlp/bin/tools/ws-server.jar')
+    }
+}
+```
+
+Within the script, then apply the plugin and specify its configuration as follows:
+
+```groovy
+apply plugin: 'liberty'
+
+liberty {
+    wlpDir = 'c:/wlp'
+    serverName = 'myServer'
+    userDir = 'c:/usr'
+    outputDir = 'c:/usr'
+}
+```
+
+Of the plugin configuration, only the `wlpDir` property is required. The default configuration is to use a server named `defaultServer` under the `build\wlp` directory of the Gradle project.
+
+The plugin will have made the following tasks available to your project:
+
+* libertyCreate - Creates a WebSphere Liberty Profile server.
+* libertyStart - Starts the WebSphere Liberty Profile server.
+* libertyRun - Runs a WebSphere Liberty Profile server under the Gradle process.
+* libertyStatus - Checks the WebSphere Liberty Profile server is running.
+* libertyPackage - Generates a WebSphere Liberty Profile server archive.
+* deployWar - Deploys a WAR file to the WebSphere Liberty Profile server.
+* undeployWar - Removes a WAR file from the WebSphere Liberty Profile server.
---
+++ src/main/groovy/com/ibm/websphere/wlp/gradle/plugins/Liberty.groovy
@@ -0,0 +1,199 @@
+/**
+ * (C) Copyright IBM Corporation 2014.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.ibm.webshere.wlp.gradle.plugins
+
+import org.gradle.api.*
+
+import com.ibm.wsspi.kernel.embeddable.Server
+import com.ibm.wsspi.kernel.embeddable.ServerBuilder
+import com.ibm.wsspi.kernel.embeddable.Server.Result
+import com.ibm.wsspi.kernel.embeddable.ServerEventListener
+import com.ibm.wsspi.kernel.embeddable.ServerEventListener.ServerEvent
+import com.ibm.wsspi.kernel.embeddable.ServerEventListener.ServerEvent.Type
+
+import java.util.concurrent.BlockingQueue
+import java.util.concurrent.LinkedBlockingQueue
+
+import org.gradle.api.logging.LogLevel
+
+class Liberty implements Plugin<Project> {
+
+    void apply(Project project) {
+
+        project.plugins.apply 'war'
+
+        project.extensions.create('liberty', LibertyExtension)
+
+        project.task('libertyRun') {
+            description = "Runs a WebSphere Liberty Profile server under the Gradle process."
+            doLast {
+                ServerBuilder builder = getServerBuilder(project);
+
+                LibertyListener listener = new LibertyListener()
+                builder.setServerEventListener(listener)
+                Result result = builder.build().start().get()
+                if (!result.successful()) throw result.getException()
+
+                while (!Type.STOPPED.equals(listener.next().getType())) {}
+            }
+        }
+
+        project.task('libertyStatus') {
+            description 'Checks the WebSphere Liberty Profile server is running.'
+            logging.level = LogLevel.INFO
+            doLast {
+                try {
+                    executeServerCommand(project, 'status', buildLibertyMap(project))
+                } catch (Exception e) {
+                    // Throws an exception if the server is stopped
+                    println e
+                }
+            }
+        }
+
+        project.task('libertyCreate') {
+            description 'Creates a WebSphere Liberty Profile server.'
+            outputs.file { new File(getUserDir(project), "servers/${project.liberty.serverName}/server.xml") }
+            logging.level = LogLevel.INFO
+            doLast {
+                executeServerCommand(project, 'create', buildLibertyMap(project))
+            }
+        }
+
+        project.task('libertyStart') {
+            description 'Starts the WebSphere Liberty Profile server.'
+            logging.level = LogLevel.INFO
+            doLast {
+                try {
+                    executeServerCommand(project, 'start', buildLibertyMap(project))
+                } catch (Exception e) {
+                    // Throws an exception if the server is already started
+                    println e
+                }
+            }
+        }
+
+        project.task('libertyStop') {
+            description 'Stops the WebSphere Liberty Profile server.'
+            logging.level = LogLevel.INFO
+            doLast {
+                try {
+                    executeServerCommand(project, 'stop', buildLibertyMap(project))
+                } catch (Exception e) {
+                    // Throws an exception if the server is already stopped
+                    println e
+                }
+            }
+        }
+        project.tasks.clean.dependsOn project.tasks.libertyStop
+
+        project.task('libertyPackage') {
+            description 'Generates a WebSphere Liberty Profile server archive.'
+            logging.level = LogLevel.INFO
+            doLast {
+                def params = buildLibertyMap(project);
+                params.put('archive', new File(project.buildDir, project.liberty.serverName + '.zip'))
+                executeServerCommand(project, 'package', params)
+            }
+        }
+
+        project.task('deployWar') {
+            description 'Deploys a WAR file to the WebSphere Liberty Profile server.'
+            logging.level = LogLevel.INFO
+            doLast {
+                def params = buildLibertyMap(project);
+                params.put('file', project.war.archivePath)
+                project.ant.taskdef(name: 'deploy', classname: 'com.ibm.websphere.wlp.ant.DeployTask') {
+                    classpath {
+                        fileset(dir: project.liberty.wlpDir + '/dev/tools/ant', includes: '*.jar')
+                    }
+                }
+                project.ant.deploy(params)
+            }
+        }
+
+        project.task('undeployWar') {
+            description 'Removes a WAR file from the WebSphere Liberty Profile server.'
+            logging.level = LogLevel.INFO
+            doLast {
+                def params = buildLibertyMap(project);
+                params.put('file', project.war.archivePath)
+                project.ant.taskdef(name: 'undeploy', classname: 'com.ibm.websphere.wlp.ant.UndeployTask') {
+                    classpath {
+                        fileset(dir: project.liberty.wlpDir + '/dev/tools/ant', includes: '*.jar')
+                    }
+                }
+                project.ant.undeploy(params)
+            }
+        }
+        
+    }
+
+    private void executeServerCommand(Project project, String command, Map<String, String> params) {
+        project.ant.taskdef(name: 'server', classname: 'com.ibm.websphere.wlp.ant.ServerTask') {
+            classpath {
+                fileset(dir: project.liberty.wlpDir + '/dev/tools/ant', includes: '*.jar')
+            }
+        }
+        params.put('operation', command)
+        project.ant.server(params)
+    }
+
+    private ServerBuilder getServerBuilder(Project project) {
+        ServerBuilder sb = new ServerBuilder()
+        sb.setName(project.liberty.serverName)
+        sb.setUserDir(getUserDir(project))
+        sb.setOutputDir(project.liberty.outputDir)
+        return sb
+    }
+
+
+    private Map<String, String> buildLibertyMap(Project project) {
+
+        Map<String, String> result = new HashMap();
+        result.put('serverName', project.liberty.serverName)
+        def libertyUserDirFile = getUserDir(project)
+        if (!libertyUserDirFile.isDirectory()) {
+            libertyUserDirFile.mkdirs()
+        }
+        result.put('userDir', libertyUserDirFile)
+        result.put('installDir', project.liberty.wlpDir)
+        result.put('outputDir', project.liberty.outputDir)
+        result.put('timeout', 300000)
+
+        return result;
+    }
+    
+    private File getUserDir(Project project) {
+        return (project.liberty.userDir == null) ? new File(project.buildDir, 'wlp') : new File(project.liberty.userDir)
+    }
+
+    private static class LibertyListener implements ServerEventListener {
+
+        private BlockingQueue<ServerEvent> queue = new LinkedBlockingQueue<ServerEvent>()
+
+        void serverEvent(ServerEvent event) {
+            queue.put(event)
+        }
+
+        ServerEvent next() {
+            return queue.take()
+        }
+
+    }
+
+
+}
