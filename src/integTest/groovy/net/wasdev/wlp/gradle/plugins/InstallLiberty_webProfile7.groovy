7b21202f0090f776eb29b48488232a6a3e2f7959
josephca <j2k@josephs-mbp.war.can.ibm.com>
2017-07-11 12:04:32 -0400

    Fix typos.

--- src/integTest/groovy/net/wasdev/wlp/gradle/plugins/InstallLiberty_webProfile7.groovy
+++ src/integTest/groovy/net/wasdev/wlp/gradle/plugins/InstallLiberty_webProfile7.groovy
@@ -25,8 +25,8 @@ import org.junit.runners.MethodSorters
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 class InstallLiberty_webProfile7 extends AbstractIntegrationTest{
     static File resourceDir = new File("build/resources/integrationTest/liberty-test")
-    static File buildDir = new File(integTestDir, "/InstallLiberty_webProfie7")
-    static String buildFilename = "install_liberty_webProfie7.gradle"
+    static File buildDir = new File(integTestDir, "/InstallLiberty_webProfile7")
+    static String buildFilename = "install_liberty_webProfile7.gradle"
 
     @BeforeClass
     public static void setup() {
