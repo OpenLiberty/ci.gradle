2a91bf388b9e05fcb7c311b321c57a9f92cb05db
jjvilleg <jjvilleg@us.ibm.com>
2017-09-29 14:27:32 -0500

    Adjusted compileJSP test ordering

--- src/integTest/groovy/net/wasdev/wlp/gradle/plugins/TestCompileJSP.groovy
+++ src/integTest/groovy/net/wasdev/wlp/gradle/plugins/TestCompileJSP.groovy
@@ -4,6 +4,12 @@ import org.junit.AfterClass
 import org.junit.BeforeClass
 import org.junit.Test
 
+import org.junit.BeforeClass
+import org.junit.FixMethodOrder
+import org.junit.Test
+import org.junit.runners.MethodSorters
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class TestCompileJSP extends AbstractIntegrationTest{
     static File resourceDir = new File("build/resources/integrationTest/sampleJSP.servlet")
     static File buildDir = new File(integTestDir, "/test-compile-jsp")
@@ -19,18 +25,18 @@ public class TestCompileJSP extends AbstractIntegrationTest{
     }
 
     @Test
-    public void test_compile_jsp() {
-        runTasks(buildDir, 'compileJsp')
-        assert new File('build/testBuilds/test-compile-jsp/build/compileJsp').exists() : 'compileJsp Directory not found!'
-    }
-
-    @Test
     public void check_for_jsp() {
         assert new File('build/testBuilds/test-compile-jsp/src/main/webapp/index.jsp').exists() : 'index.jsp not found!'
     }
 
     @Test
-    public void compile_jsp_class_file() {
+    public void test_1() {
+        runTasks(buildDir, 'compileJsp')
+        assert new File('build/testBuilds/test-compile-jsp/build/compileJsp').exists() : 'compileJsp Directory not found!'
+    }
+
+    @Test
+    public void test_2() {
         assert new File('build/testBuilds/test-compile-jsp/build/classes/java/_index.class').exists() : '_index.class not found!'
     }
 }
