bca3a11a6eae0f78127c22e3b743d27e09b9c44a
jjvilleg <jjvilleg@us.ibm.com>
2017-10-09 14:56:10 -0500

    Liberty package now works with looseApp changes

---
+++ src/integTest/resources/liberty-package-test/src/main/java/application/servlet/Thing.java
@@ -0,0 +1,51 @@
+package application.servlet;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Thing implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column
+    private String description;
+
+    @Column
+    private int quantity;
+
+    public Thing() {}
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "Thing[" + id + "]";
+    }
+}
