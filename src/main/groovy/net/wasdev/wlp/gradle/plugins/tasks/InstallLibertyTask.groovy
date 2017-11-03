776cc277e09d58e1a659d6c0e3a4e7ad34aa852d
mattbsox <m.bowersox@ibm.com>
2017-11-03 12:54:35 -0500

    removed unused import

--- src/main/groovy/net/wasdev/wlp/gradle/plugins/tasks/InstallLibertyTask.groovy
+++ src/main/groovy/net/wasdev/wlp/gradle/plugins/tasks/InstallLibertyTask.groovy
@@ -19,7 +19,6 @@ import javax.xml.parsers.*
 
 import org.gradle.api.Project
 import org.gradle.api.tasks.TaskAction
-import net.wasdev.wlp.gradle.plugins.utils.ApplicationXmlDocument
 import groovy.xml.MarkupBuilder
 
 class InstallLibertyTask extends AbstractTask {
