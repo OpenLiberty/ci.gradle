812fe90ef51ac9118bd58d29e3b9ef77e86c02da
Charles Tian <ctian@us.ibm.com>
2017-12-14 14:39:35 -0600

    Fix formatting, update to use new version of ci.common

--- src/main/groovy/net/wasdev/wlp/gradle/plugins/tasks/extensions/arquillian/ConfigureArquillianTask.groovy
+++ src/main/groovy/net/wasdev/wlp/gradle/plugins/tasks/extensions/arquillian/ConfigureArquillianTask.groovy
@@ -14,55 +14,54 @@ import org.gradle.api.tasks.TaskAction
 import org.xml.sax.SAXException
 
 import net.wasdev.wlp.common.arquillian.objects.LibertyManagedObject
-import net.wasdev.wlp.common.arquillian.objects.LibertyManagedProperty
+import net.wasdev.wlp.common.arquillian.objects.LibertyProperty
 import net.wasdev.wlp.common.arquillian.util.ArquillianConfigurationException
 import net.wasdev.wlp.common.arquillian.util.Constants
 import net.wasdev.wlp.common.arquillian.util.HttpPortUtil
 import net.wasdev.wlp.gradle.plugins.tasks.AbstractServerTask
 
 class ConfigureArquillianTask extends AbstractServerTask {
-	
-	@Input
-	public boolean skipIfArquillianXmlExists = false;
-	
-	@Input
-	public Map<String, String> arquillianProperties = null;
-	
-	@TaskAction
-    void doExecute() throws GradleException {
-		File arquillianXml = new File(project.getBuildDir(), "resources/test/arquillian.xml");
-		if (skipIfArquillianXmlExists && arquillianXml.exists()) {
-			logger.info(
-					"Skipping configure-arquillian task because arquillian.xml already exists in \"target/test-classes\".");
-		} else {
-			configureArquillian(arquillianXml);
-		}
-	}
-	
-	private void configureArquillian(File arquillianXml) throws GradleException {
-		try {
-			LibertyManagedObject arquillianManaged = new LibertyManagedObject(getInstallDir(project).getCanonicalPath(), getServer().name,
-				getHttpPort(), LibertyManagedProperty.getArquillianProperties(arquillianProperties));
-			arquillianManaged.build(arquillianXml);
-		} catch(Exception e) {
-			throw new GradleException("Error configuring Arquillian.", e);
-		}
-	}
-	
-	/**
-	 * @return the HTTP port that the managed Liberty server is running on.
-	 * @throws SAXException
-	 * @throws ParserConfigurationException
-	 * @throws IOException
-	 * @throws XPathExpressionException
-	 * @throws FileNotFoundException
-	 * @throws ArquillianConfigurationException
-	 */
-	private int getHttpPort() throws FileNotFoundException, XPathExpressionException, IOException, ParserConfigurationException, SAXException, ArquillianConfigurationException {
-		String serverDirectory = getServerDir(project);
-		File serverXML = new File(serverDirectory + "/server.xml");
-		File bootstrapProperties = new File(serverDirectory + "/bootstrap.properties");
-		return HttpPortUtil.getHttpPort(serverXML, bootstrapProperties);
-	}
 
+    @Input
+    public boolean skipIfArquillianXmlExists = false;
+
+    @Input
+    public Map<String, String> arquillianProperties = null;
+
+    @TaskAction
+    void doExecute() throws GradleException {
+        File arquillianXml = new File(project.getBuildDir(), "resources/test/arquillian.xml");
+        if (skipIfArquillianXmlExists && arquillianXml.exists()) {
+            logger.info(
+                    "Skipping configure-arquillian task because arquillian.xml already exists in \"target/test-classes\".");
+        } else {
+            configureArquillian(arquillianXml);
+        }
+    }
+
+    private void configureArquillian(File arquillianXml) throws GradleException {
+        try {
+            LibertyManagedObject arquillianManaged = new LibertyManagedObject(getInstallDir(project).getCanonicalPath(), getServer().name,
+                    getHttpPort(), LibertyProperty.getArquillianProperties(arquillianProperties, LibertyManagedObject.LibertyManagedProperty.class));
+            arquillianManaged.build(arquillianXml);
+        } catch(Exception e) {
+            throw new GradleException("Error configuring Arquillian.", e);
+        }
+    }
+
+    /**
+     * @return the HTTP port that the managed Liberty server is running on.
+     * @throws SAXException
+     * @throws ParserConfigurationException
+     * @throws IOException
+     * @throws XPathExpressionException
+     * @throws FileNotFoundException
+     * @throws ArquillianConfigurationException
+     */
+    private int getHttpPort() throws FileNotFoundException, XPathExpressionException, IOException, ParserConfigurationException, SAXException, ArquillianConfigurationException {
+        String serverDirectory = getServerDir(project);
+        File serverXML = new File(serverDirectory + "/server.xml");
+        File bootstrapProperties = new File(serverDirectory + "/bootstrap.properties");
+        return HttpPortUtil.getHttpPort(serverXML, bootstrapProperties);
+    }
 }
