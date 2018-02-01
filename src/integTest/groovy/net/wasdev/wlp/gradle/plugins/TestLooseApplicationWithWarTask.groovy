aaf17e5a56f08be478665bac4eede9f77f2f77b4
jjvilleg <jjvilleg@us.ibm.com>
2018-02-01 11:30:55 -0600

    Loose Application for Ear Support (#202)
    * Adding loose app ear supp
    
    * Jar and war support added. Need to refactor jar and add more war support
    
    * Added looseApp ear support and added supporting tests
    
    * removed .factorypath and extra .gitignores
    
    * Adding updates from pr
    
    * Changed order of subproject dependencies
    
    * Trying to fix test with wrong order
    
    * Still trying to fix tests
    
    * Updating travis yml
    
    * Updating yml still
    
    * Adding sudo environ in travis yml
    
    * Updated jdk to only use openjdk8
    
    * Updated tests to be mroe dynamic in ordering
    
    * Tests should be working now

--- src/integTest/groovy/net/wasdev/wlp/gradle/plugins/TestLooseApplicationWithWarTask.groovy
+++ src/integTest/groovy/net/wasdev/wlp/gradle/plugins/TestLooseApplicationWithWarTask.groovy
@@ -82,7 +82,7 @@ public class TestLooseApplicationWithWarTask extends AbstractIntegrationTest{
 
       expression = "/archive/file";
       nodes = (NodeList) xPath.compile(expression).evaluate(inputDoc, XPathConstants.NODESET);
-      Assert.assertEquals("Number of <file/> element ==>", 2, nodes.getLength());
+      Assert.assertEquals("Number of <file/> element ==>", 3, nodes.getLength());
 
       Assert.assertEquals("archive targetInArchive attribute value", "/WEB-INF/lib/commons-text-1.1.jar",
               nodes.item(0).getAttributes().getNamedItem("targetInArchive").getNodeValue());
