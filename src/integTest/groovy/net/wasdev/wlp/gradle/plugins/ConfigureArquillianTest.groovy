4c11007b1405ea01b38933a3b57df25f533e0790
mattbsox <m.bowersox@ibm.com>
2018-03-20 11:35:23 -0500

    adding more test parameters to properties file

--- src/integTest/groovy/net/wasdev/wlp/gradle/plugins/ConfigureArquillianTest.groovy
+++ src/integTest/groovy/net/wasdev/wlp/gradle/plugins/ConfigureArquillianTest.groovy
@@ -24,7 +24,7 @@ class ConfigureArquillianTest extends AbstractIntegrationTest {
     public static void setup() {
         createDir(buildDir)
         FileUtils.copyDirectory(resourceDir, buildDir);
-        copyFile(new File("build/resources/integrationTest/gradle.properties"), new File(buildDir, 'gradle.properties'))
+        copyFile(new File("gradle.properties"), new File(buildDir, 'gradle.properties'))
     }
 
     @Test
