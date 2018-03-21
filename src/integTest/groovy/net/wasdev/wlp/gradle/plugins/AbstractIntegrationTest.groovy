816c84b895b170abfc044bf9952a27db69dae062
Matt Bowersox <m.bowersox@ibm.com>
2018-03-21 13:39:51 -0500

    adding more test parameters to properties file (#240)

--- src/integTest/groovy/net/wasdev/wlp/gradle/plugins/AbstractIntegrationTest.groovy
+++ src/integTest/groovy/net/wasdev/wlp/gradle/plugins/AbstractIntegrationTest.groovy
@@ -47,7 +47,7 @@ abstract class AbstractIntegrationTest {
 
     protected static File copyBuildFiles(File buildFilename, File buildDir) {
             copyFile(buildFilename, new File(buildDir, 'build.gradle'))
-            copyFile(new File("build/resources/integrationTest/gradle.properties"), new File(buildDir, 'gradle.properties'))
+            copyFile(new File("gradle.properties"), new File(buildDir, 'gradle.properties'))
     }
 
     protected static File createTestProject(File parent, File sourceDir, String buildFilename) {
@@ -68,7 +68,7 @@ abstract class AbstractIntegrationTest {
             // copy the needed gradle build and property files
             File sourceFile = new File(sourceDir, buildFilename)
             copyBuildFiles(sourceFile, parent)
-            
+
         } catch (IOException e) {
             throw new AssertionError("Unable to copy directory '${parent.canonicalPath}'.")
         }
