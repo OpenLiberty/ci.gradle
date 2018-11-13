f9cd29a882c003597f15ed3f02aafe423c6642b9
Juan Villegas <jjvilleg@us.ibm.com>
2018-11-13 15:22:38 -0600

    Resolving Deprecation Warning from using WebPluginConvnetion Constructor (#285)
    * Working on removing soon to be depracated WarPluginConvention reference
    
    * Getting WarPluginConvention from war plugin instead of depracated constructor

--- src/main/groovy/net/wasdev/wlp/gradle/plugins/utils/LooseWarApplication.groovy
+++ src/main/groovy/net/wasdev/wlp/gradle/plugins/utils/LooseWarApplication.groovy
@@ -3,19 +3,20 @@ package net.wasdev.wlp.gradle.plugins.utils;
 import java.io.File;
 import org.gradle.api.Project;
 import org.gradle.api.plugins.WarPluginConvention
+import org.gradle.api.plugins.War
 import org.gradle.api.Task
 import org.gradle.api.tasks.bundling.War
 
 public class LooseWarApplication extends LooseApplication {
 
     public LooseWarApplication(Task task, LooseConfigData config) {
-        super(task, config);
+        super(task, config)
     }
 
     public void addSourceDir() throws Exception {
-        WarPluginConvention wpc = new WarPluginConvention(task.getProject())
-        File sourceDir = new File(wpc.getWebAppDir().getAbsolutePath());
-        config.addDir(sourceDir.getCanonicalPath(), "/");
+        WarPluginConvention wpc = task.getProject().getConvention().findPlugin(WarPluginConvention)
+        File sourceDir = new File(wpc.getWebAppDir().getAbsolutePath())
+        config.addDir(sourceDir.getCanonicalPath(), "/")
     }
 
 
