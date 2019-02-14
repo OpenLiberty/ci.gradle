c0a6fba787d20d0a968960ab96a54e7dc69524c1
mattbsox <m.bowersox@ibm.com>
2019-02-14 13:27:12 -0600

    Adding libertyApplication configuration to pull in application artifacts

---
+++ src/integTest/groovy/net/wasdev/wlp/gradle/plugins/LibertyApplicationConfigurationM2Test.groovy
@@ -0,0 +1,46 @@
+/*
+ * (C) Copyright IBM Corporation 2019.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.wasdev.wlp.gradle.plugins
+
+import static org.junit.Assert.*
+
+import org.junit.BeforeClass
+import org.junit.Test
+
+class LibertyApplicationConfigurationM2Test extends AbstractIntegrationTest {
+    static File resourceDir = new File("build/resources/integrationTest/app-configuration-m2-test")
+    static File buildDir = new File(integTestDir, "/LibertyApplicationConfigurationM2Test")
+    static String buildFilename = "appConfigurationM2Test.gradle"
+
+    @BeforeClass
+    public static void setup() {
+        createDir(buildDir)
+        createTestProject(buildDir, resourceDir, buildFilename)
+        try {
+            //Installing the war built by the other gradle project in the src dir
+            runTasks(new File(buildDir, 'test-maven-war'), 'install')
+            //Then installing that war from m2 to the apps directory through the libertyApplication configuration
+            runTasks(buildDir, 'installApps')
+        } catch (Exception e) {
+            throw new AssertionError ("Fail on task installApps. "+ e)
+        }
+    }
+
+    @Test
+    public void checkAppInstalled() {
+        assert new File(buildDir, 'build/wlp/usr/servers/defaultServer/apps/test-maven-war-1.0-SNAPSHOT.war').exists()
+    }
+}
