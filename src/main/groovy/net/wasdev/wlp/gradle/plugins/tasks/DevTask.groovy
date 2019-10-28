d039c892bcbd0a839726ca87ffdb83a77b8bdb1e
rzgry <ryan.zegray@gmail.com>
2019-10-28 11:47:39 -0400

    remove extra call to serverTask.execute()

--- src/main/groovy/net/wasdev/wlp/gradle/plugins/tasks/DevTask.groovy
+++ src/main/groovy/net/wasdev/wlp/gradle/plugins/tasks/DevTask.groovy
@@ -262,7 +262,6 @@ class DevTask extends AbstractServerTask {
             copyConfigFiles();
             serverTask.setUseEmbeddedServer(server.embedded)
             serverTask.setClean(server.clean)
-            serverTask.execute()
             return serverTask;
         }
 
