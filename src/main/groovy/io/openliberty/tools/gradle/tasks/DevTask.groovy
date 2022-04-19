e10f8a00c1b2e1ac74dca715dbeb66a498d41220
Kathryn Kodama <kathryn.s.kodama@gmail.com>
2022-04-18 21:34:53 -0400

    Display warning if installFeature fails when generateFeatures is enabled and Liberty feature deps are in the build file (#733)
    * Display warning if installFeature fails when generateFeatures is enabled and Liberty feature deps are in the build file
    
    Signed-off-by: Kathryn Kodama <kathryn.s.kodama@gmail.com>
    
    * Check for feature conflict when install feature fails
    
    Signed-off-by: Kathryn Kodama <kathryn.s.kodama@gmail.com>
    
    * Place conflict message check inside of if statement logic for Liberty feature dep warning
    
    Signed-off-by: Kathryn Kodama <kathryn.s.kodama@gmail.com>

--- src/main/groovy/io/openliberty/tools/gradle/tasks/DevTask.groovy
+++ src/main/groovy/io/openliberty/tools/gradle/tasks/DevTask.groovy
@@ -37,6 +37,7 @@ import java.util.concurrent.ThreadPoolExecutor
 import io.openliberty.tools.ant.ServerTask
 
 import io.openliberty.tools.common.plugins.util.DevUtil
+import io.openliberty.tools.common.plugins.util.InstallFeatureUtil;
 import io.openliberty.tools.common.plugins.util.PluginExecutionException
 import io.openliberty.tools.common.plugins.util.PluginScenarioException
 import io.openliberty.tools.common.plugins.util.ServerFeatureUtil
@@ -656,7 +657,7 @@ class DevTask extends AbstractFeatureTask {
 
 
         @Override
-        public void installFeatures(File configFile, File serverDir) {
+        public void installFeatures(File configFile, File serverDir, boolean generateFeatures) {
             ServerFeatureUtil servUtil = getServerFeatureUtil(true);
             Set<String> features = servUtil.getServerFeatures(serverDir, libertyDirPropertyFiles);
 
@@ -698,7 +699,17 @@ class DevTask extends AbstractFeatureTask {
                 } catch (BuildException e) {
                     // stdout/stderr from the installFeature task is sent to the terminal
                     // only need to log the actual stacktrace when debugging
-                    logger.debug('Failed to install features from configuration file', e);
+                    logger.error('Failed to install features from configuration file', e);
+                    if (generateFeatures && !project.configurations.getByName('libertyFeature').dependencies.isEmpty()) {
+                        if (e.getCause() != null && e.getCause().getCause() != null && e.getCause().getCause().getCause() != null) {
+                            // PluginExecutionException from installFeature will be 3 layers deep
+                            if (e.getCause().getCause().getCause().getMessage().contains(InstallFeatureUtil.CONFLICT_MESSAGE)) {
+                                logger.warn("Liberty feature dependencies were detected in the build.gradle file and automatic generation of features is [On]. "
+                                        + "Automatic generation of features does not support Liberty feature dependencies. "
+                                        + "Remove any Liberty feature dependencies from the build.gradle file or disable automatic generation of features by typing 'g' and press Enter.");
+                            }
+                        }
+                    }
                 } finally {
                     gradleConnection.close();
                 }
