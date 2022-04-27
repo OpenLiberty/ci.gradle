acb5b1273059a914169072877bb66d575fbfe56e
Kathryn Kodama <kathryn.s.kodama@gmail.com>
2022-04-27 17:30:27 -0400

    Display Liberty feature dependency warning if install feature fails on build file change (#744)
    Signed-off-by: Kathryn Kodama <kathryn.s.kodama@gmail.com>

--- src/main/groovy/io/openliberty/tools/gradle/tasks/DevTask.groovy
+++ src/main/groovy/io/openliberty/tools/gradle/tasks/DevTask.groovy
@@ -57,6 +57,9 @@ class DevTask extends AbstractFeatureTask {
     private static final String MICROSHED_HTTP_PORT = "microshed_http_port";
     private static final String MICROSHED_HTTPS_PORT = "microshed_https_port";
     private static final String WLP_USER_DIR_PROPERTY_NAME = "wlp.user.dir";
+    private static final String GEN_FEAT_LIBERTY_DEP_WARNING = "Liberty feature dependencies were detected in the build.gradle file and automatic generation of features is [On]. " +
+            "Automatic generation of features does not support Liberty feature dependencies. " +
+            "Remove any Liberty feature dependencies from the build.gradle file or disable automatic generation of features by typing 'g' and press Enter.";
 
     DevTask() {
         configure({
@@ -592,9 +595,28 @@ class DevTask extends AbstractFeatureTask {
                 util.restartServer();
                 return true;
             } else if (installFeatures) {
-                libertyInstallFeature();
+                try {
+                    libertyInstallFeature();
+                } catch (PluginExecutionException e) {
+                    // display warning if install feature fails, generateFeatures is on, and Liberty features are in buildfile
+                    if (e.getCause() instanceof BuildException && generateFeatures) {
+                        libertyDependencyWarning(e.getCause());
+                    }
+                }
             }
             return true;
+        }
+
+        // Check if BuildException contains InstallFeature feature conflict error message
+        // This method is only meant to be called if generateFeatures == true and Liberty feature dependencies
+        // are detected in the build file
+        private void libertyDependencyWarning(BuildException e) {
+            if (e.getCause() != null && e.getCause().getCause() != null && e.getCause().getCause().getCause() != null) {
+                // PluginExecutionException from installFeature will be 3 layers deep
+                if (e.getCause().getCause().getCause().getMessage().contains(InstallFeatureUtil.CONFLICT_MESSAGE)) {
+                    logger.warn(GEN_FEAT_LIBERTY_DEP_WARNING);
+                }
+            }
         }
 
         private boolean hasServerConfigBootstrapPropertiesChanged(Project newProject, Project oldProject) {
@@ -699,16 +721,9 @@ class DevTask extends AbstractFeatureTask {
                 } catch (BuildException e) {
                     // stdout/stderr from the installFeature task is sent to the terminal
                     // only need to log the actual stacktrace when debugging
-                    logger.error('Failed to install features from configuration file', e);
+                    logger.error('Failed to install features from configuration file' + e.getMessage())
                     if (generateFeatures && !project.configurations.getByName('libertyFeature').dependencies.isEmpty()) {
-                        if (e.getCause() != null && e.getCause().getCause() != null && e.getCause().getCause().getCause() != null) {
-                            // PluginExecutionException from installFeature will be 3 layers deep
-                            if (e.getCause().getCause().getCause().getMessage().contains(InstallFeatureUtil.CONFLICT_MESSAGE)) {
-                                logger.warn("Liberty feature dependencies were detected in the build.gradle file and automatic generation of features is [On]. "
-                                        + "Automatic generation of features does not support Liberty feature dependencies. "
-                                        + "Remove any Liberty feature dependencies from the build.gradle file or disable automatic generation of features by typing 'g' and press Enter.");
-                            }
-                        }
+                        libertyDependencyWarning(e);
                     }
                 } finally {
                     gradleConnection.close();
@@ -857,7 +872,6 @@ class DevTask extends AbstractFeatureTask {
         public void libertyInstallFeature() {
             ProjectConnection gradleConnection = initGradleProjectConnection();
             BuildLauncher gradleBuildLauncher = gradleConnection.newBuild();
-
             try {
                 List<String> options = new ArrayList<String>();
                 if (container) {
