ec468e0fae2fce3a80eb8714547bd0ef327b2fe3
Cheryl King <cheryllk@us.ibm.com>
2023-05-31 13:49:50 -0500

    Improve upToDate check for installLiberty (#826)

--- src/test/groovy/io/openliberty/tools/gradle/InstallLiberty_runtimeDep_upToDate_Test.groovy
+++ src/test/groovy/io/openliberty/tools/gradle/InstallLiberty_runtimeDep_upToDate_Test.groovy
@@ -27,6 +27,7 @@ class InstallLiberty_runtimeDep_upToDate_Test extends AbstractIntegrationTest{
     static File resourceDir = new File("build/resources/test/liberty-test")
     static File buildDir = new File(integTestDir, "/InstallLiberty_upToDate_Test")
     static String runTimeDep_UpToDate_buildFilename = "installLiberty_upToDate_runtimeDep_Test.gradle"
+    static File wsLaunchJar = new File(buildDir, "build/wlp/lib/ws-launch.jar")
 
     @BeforeClass
     public static void setup() {
@@ -41,7 +42,18 @@ class InstallLiberty_runtimeDep_upToDate_Test extends AbstractIntegrationTest{
 
     @Test
     public void test_installLiberty_upToDate() {
+        // same version as installed in the setup method, so upToDate check should return true
         assert runTaskCheckForUpToDate(buildDir, 'installLiberty', "-PlibertyVersion=21.0.0.1")
+
+        // changing the libertyVersion causes the upToDate check to return false
         assertFalse runTaskCheckForUpToDate(buildDir, 'installLiberty', "-PlibertyVersion=21.0.0.2")
+
+        // verify upToDate check returns true for same libertyVersion
+        assert runTaskCheckForUpToDate(buildDir, 'installLiberty', "-PlibertyVersion=21.0.0.2")
+
+        // now delete the ws-launch.jar from the installation and ensure the upToDate check returns false even though the libertyVersion did not change
+        wsLaunchJar.delete()
+        assertFalse runTaskCheckForUpToDate(buildDir, 'installLiberty', "-PlibertyVersion=21.0.0.2")
+
     }
 }
