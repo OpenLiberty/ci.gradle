2d34dc807b9ef46baa508179fd3f43e7920f51c8
Cheryl King <cheryllk@us.ibm.com>
2025-02-24 14:45:16 -0600

    Add documentation to SpringBootExtension

--- src/main/groovy/io/openliberty/tools/gradle/extensions/SpringBootExtension.groovy
+++ src/main/groovy/io/openliberty/tools/gradle/extensions/SpringBootExtension.groovy
@@ -1,5 +1,5 @@
 /**
- * (C) Copyright IBM Corporation 2018, 2019.
+ * (C) Copyright IBM Corporation 2018, 2025.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,8 +16,17 @@
 package io.openliberty.tools.gradle.extensions
 
 class SpringBootExtension {
+    // The path of the source application file to thin.
     String sourceAppPath
+
+    // The directory path of the parent read-only library cache. The parent library cache is searched first to locate existing libraries. 
+    // If the library is not found, the library is stored in the writable library cache that is specified by the `targetLibCachePath` option. 
+    // If this option is not specified, no parent library cache is searched.
     String parentLibCachePath
+
+    // The directory path that is used to save the library cache. If this option is not specified, a `lib.index.cache` directory is created in the parent directory of the source application.
     String targetLibCachePath
+
+    // The path that is used to save the thin application file. If this option is not specified, a new file is created with the `.spring` extension in the parent directory of the source application.
     String targetThinAppPath
 }
