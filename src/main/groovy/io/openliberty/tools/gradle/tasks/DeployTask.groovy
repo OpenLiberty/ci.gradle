37eecd3b77e15a0536004a355a6a90597ad3a4e5
Sajeer <sajeer@Sajeers-MacBook-Pro.local>
2025-08-22 11:43:07 +0530

    sourceCompatibility and targetCompatibility syntax changes and method visibility changes

--- src/main/groovy/io/openliberty/tools/gradle/tasks/DeployTask.groovy
+++ src/main/groovy/io/openliberty/tools/gradle/tasks/DeployTask.groovy
@@ -122,7 +122,7 @@ class DeployTask extends AbstractServerTask {
         }
     }
 
-    private void installProjectArchive(Task task, String appsDir) {
+    protected void installProjectArchive(Task task, String appsDir) {
         String archiveBaseName
         String fileName
         if("springboot".equals(getPackagingType())) {
@@ -578,7 +578,7 @@ class DeployTask extends AbstractServerTask {
         }
     }
 
-    private void addLibrary(Element parent, LooseApplication looseApp, String dir, File lib) throws GradleException {
+    protected void addLibrary(Element parent, LooseApplication looseApp, String dir, File lib) throws GradleException {
         if(server.deploy.copyLibsDirectory != null) {
             if(!server.deploy.copyLibsDirectory.exists()) {
                 server.deploy.copyLibsDirectory.mkdirs()
