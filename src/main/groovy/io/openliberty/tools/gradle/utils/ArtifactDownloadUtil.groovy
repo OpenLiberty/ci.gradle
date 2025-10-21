9b9542e421ea7606bba953cbedf3a699dacac2be
Arun Venmany <Arun.Kumar.V.N@ibm.com>
2025-10-21 17:23:59 +0530

    ArtifactDownloadUtil modified. Added tests for getDependencyFeatures
    Signed-off-by: Arun Venmany <Arun.Kumar.V.N@ibm.com>

--- src/main/groovy/io/openliberty/tools/gradle/utils/ArtifactDownloadUtil.groovy
+++ src/main/groovy/io/openliberty/tools/gradle/utils/ArtifactDownloadUtil.groovy
@@ -1,5 +1,5 @@
 /**
- * (C) Copyright IBM Corporation 2021.
+ * (C) Copyright IBM Corporation 2021, 2025.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,6 +15,7 @@
  */
 package io.openliberty.tools.gradle.utils
 
+import io.openliberty.tools.common.plugins.util.VariableUtility
 import org.apache.commons.io.FileUtils
 import org.gradle.api.Project
 import org.gradle.api.artifacts.ResolveException
@@ -23,7 +24,7 @@ import org.gradle.internal.resolve.ArtifactNotFoundException
 
 public class ArtifactDownloadUtil {
     public static File downloadArtifact(Project project, String groupId, String artifactId, String type, String version) throws PluginExecutionException {
-        String coordinates = groupId + ":" + artifactId + ":" + version + "@" + type
+        String coordinates = getResolvedCoordinates(project, groupId, artifactId, version, type)
         def dep = project.dependencies.create(coordinates)
         def config = project.configurations.detachedConfiguration(dep)
 
@@ -31,7 +32,7 @@ public class ArtifactDownloadUtil {
     }
 
     public static File downloadBuildArtifact(Project project, String groupId, String artifactId, String type, String version) throws PluginExecutionException {
-        String coordinates = groupId + ":" + artifactId + ":" + version + "@" + type
+        String coordinates = getResolvedCoordinates(project, groupId, artifactId, version, type)
         def dep = project.buildscript.dependencies.create(coordinates)
         def config = project.buildscript.configurations.detachedConfiguration(dep)
 
@@ -39,7 +40,7 @@ public class ArtifactDownloadUtil {
     }
 	
 	public static File downloadSignature(Project project, String groupId, String artifactId, String type, String version, File esa) throws PluginExecutionException {
-        String coordinates = groupId + ":" + artifactId + ":" + version + "@" + type
+        String coordinates = getResolvedCoordinates(project, groupId, artifactId, version, type)
 		def dep = project.dependencies.create(coordinates)
 		def config = project.configurations.detachedConfiguration(dep)
 		def sig = downloadFile(project, config, coordinates);
@@ -68,5 +69,24 @@ public class ArtifactDownloadUtil {
             throw new PluginExecutionException("Could not find artifact with coordinates " + coordinates)
         }
         return files.iterator().next()
+    }
+
+    /**
+     * get coordinates after resolving any variables
+     * @param project
+     * @param groupId
+     * @param artifactId
+     * @param version
+     * @param type
+     * @return
+     */
+    public static String getResolvedCoordinates(Project project, String groupId, String artifactId, String version, String type) {
+        Properties projectProps = new Properties();
+        project.properties.entrySet().forEach { p -> if (p.value != null) { projectProps.put(p.key, p.value) } }
+        String resolvedGroupId = VariableUtility.resolveVariables(new CommonLogger(project), groupId, Collections.emptyList(), projectProps, new Properties(), Collections.emptyMap())
+        String resolvedArtifactId = VariableUtility.resolveVariables(new CommonLogger(project), artifactId, Collections.emptyList(), projectProps, new Properties(), Collections.emptyMap())
+        String resolvedVersion = VariableUtility.resolveVariables(new CommonLogger(project), version, Collections.emptyList(), projectProps, new Properties(), Collections.emptyMap())
+        String resolvedType = VariableUtility.resolveVariables(new CommonLogger(project), type, Collections.emptyList(), projectProps, new Properties(), Collections.emptyMap())
+        return resolvedGroupId + ":" + resolvedArtifactId + ":" + resolvedVersion + "@" + resolvedType
     }
 }
