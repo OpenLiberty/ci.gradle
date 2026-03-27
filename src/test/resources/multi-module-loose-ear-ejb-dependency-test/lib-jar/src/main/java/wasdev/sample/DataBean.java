0d44cfd4dc585e12cc401f9cecff291208bd3aee
Sajeer <sajeer@Sajeers-MacBook-Pro.local>
2026-03-27 18:50:23 +0530

    EJB Dependency Fixes

---
+++ src/test/resources/multi-module-loose-ear-ejb-dependency-test/lib-jar/src/main/java/wasdev/sample/DataBean.java
@@ -0,0 +1,20 @@
+package wasdev.sample;
+
+public class DataBean {
+    private String value;
+    
+    public DataBean() {
+    }
+    
+    public DataBean(String value) {
+        this.value = value;
+    }
+    
+    public String getValue() {
+        return value;
+    }
+    
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
