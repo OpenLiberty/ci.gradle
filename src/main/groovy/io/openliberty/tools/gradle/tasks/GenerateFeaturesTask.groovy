e839b269245800ce4100303c1322f455af63b1c2
Paul Gooderham <turkeyonmarblerye@gmail.com>
2026-05-08 17:24:26 -0400

    Clarify message for failure to write configDocument
    Signed-off-by: Paul Gooderham <turkeyonmarblerye@gmail.com>

--- src/main/groovy/io/openliberty/tools/gradle/tasks/GenerateFeaturesTask.groovy
+++ src/main/groovy/io/openliberty/tools/gradle/tasks/GenerateFeaturesTask.groovy
@@ -343,7 +343,7 @@ class GenerateFeaturesTask extends AbstractFeatureTask {
             } catch (TransformerException | IOException e) {
                 logger.warn("Failed to write generated-features.xml to server directory: "
                     + serverDirXmlFile.getAbsolutePath() + ". Ensure your ID has write permission to the server configuration directory. "
-                    + e.getMessage());
+                    + "Message from the Exception: " + e.getMessage());
                 return false;
             }
         }
