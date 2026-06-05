b5d874a3cd372538e8aef179cc63886310e9b959
Arunvenmany <arun.kumar.v.n@ibm.com>
2026-06-05 08:43:42 +0530

    tests added for spring boot 4 (#1087)
    * tests added for spring boot 4
    
    * updating runtimeVersion
    
    * added validation check for spring boot 4.0 liberty runtime requirement
    
    * changing workflow to use ci.common main branch
    
    * set up a Map with springboot version as key and minimum Liberty version

--- src/main/groovy/io/openliberty/tools/gradle/tasks/AbstractLibertyTask.groovy
+++ src/main/groovy/io/openliberty/tools/gradle/tasks/AbstractLibertyTask.groovy
@@ -15,6 +15,7 @@
  */
 package io.openliberty.tools.gradle.tasks
 
+import io.openliberty.tools.common.plugins.util.VersionUtility
 import io.openliberty.tools.gradle.Liberty
 import org.gradle.api.DefaultTask
 import org.gradle.api.GradleException
@@ -33,6 +34,8 @@ import org.gradle.jvm.toolchain.JavaToolchainService
 import javax.inject.Inject
 
 abstract class AbstractLibertyTask extends DefaultTask {
+    protected static final Map<Integer, String> MIN_LIBERTY_VERSION_BY_SPRING_BOOT_MAJOR_VERSION =
+            [4: "26.0.0.5"].asImmutable()
 
     //params that get built with installLiberty
     protected def params
@@ -187,6 +190,35 @@ abstract class AbstractLibertyTask extends DefaultTask {
             return "springBoot-"+majorVersion+".0"
         }
         return null
+    }
+
+    protected static Integer getSpringBootMajorVersion(String springBootVersion) {
+        if (!isSpringBoot2plus(springBootVersion)) {
+            return null
+        }
+        String majorVersion = springBootVersion.substring(0, springBootVersion.indexOf('.'))
+        try {
+            return Integer.parseInt(majorVersion)
+        } catch (NumberFormatException e) {
+            return null
+        }
+    }
+
+    protected static String getSpringBootMinimumLibertyVersion(String springBootVersion) {
+        Integer springBootMajorVersion = getSpringBootMajorVersion(springBootVersion)
+        return springBootMajorVersion == null ? null : MIN_LIBERTY_VERSION_BY_SPRING_BOOT_MAJOR_VERSION.get(springBootMajorVersion)
+    }
+
+    protected void validateSpringBootLibertyVersion() {
+        String detectedSpringBootVersion = springBootVersion ?: findSpringBootVersion(project)
+        String minimumVersion = getSpringBootMinimumLibertyVersion(detectedSpringBootVersion)
+        if (minimumVersion == null) {
+            return
+        }
+        String libertyVersion = getLibertyInstallProperties().getProperty(COM_IBM_WEBSPHERE_PRODUCTVERSION_KEY)
+        if (VersionUtility.compareArtifactVersion(libertyVersion, minimumVersion, true) < 0) {
+            throw new GradleException("Spring Boot ${detectedSpringBootVersion} applications require Liberty runtime version ${minimumVersion} or later. The configured Liberty runtime version is ${libertyVersion}.")
+        }
     }
 
     protected static Task findSpringBootTask(Project project, String springBootVersion) {
---
+++ src/test/groovy/io/openliberty/tools/gradle/TestSpringBootApplication40.groovy
@@ -0,0 +1,252 @@
+/*
+ * (C) Copyright IBM Corporation 2026.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.openliberty.tools.gradle
+
+import org.gradle.testkit.runner.BuildResult
+import org.junit.*
+import org.junit.rules.TestName
+
+import static org.junit.Assert.assertTrue
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+
+public class TestSpringBootApplication40 extends AbstractIntegrationTest{
+    static File resourceDir = new File("build/resources/test/sample.springboot4")
+
+    File buildDir;
+
+    @Rule
+    public TestName testName = new TestName();
+
+    @Before
+    public void setup() {
+        buildDir = new File(integTestDir, "/" + testName.getMethodName())
+        createDir(buildDir)
+        createTestProject(buildDir, resourceDir, testName.getMethodName() + '.gradle')
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        File buildFile = new File(buildDir, 'build.gradle')
+        if (buildFile.exists()) {
+            String buildContents = buildFile.getText('UTF-8')
+            if (buildContents.contains('liberty')) {
+                try {
+                    runTasks(buildDir,'libertyStop')
+                } catch (Exception e) {
+                    // ignore cleanup failures for tests that fail before the liberty tasks are available
+                }
+            }
+        }
+    }
+
+
+    @Test
+    public void test_spring_boot_apps_40() {
+        try {
+            runTasks(buildDir, 'deploy', 'libertyStart')
+
+            String webPage = new URL("http://localhost:9080").getText()
+            Assert.assertEquals("Did not get expected http response.","Hello!", webPage)
+            Assert.assertTrue('defaultServer/dropins has app deployed',
+                    new File(buildDir, 'build/wlp/usr/servers/defaultServer/dropins').list().size() == 0)
+            Assert.assertTrue('defaultServer/configDropins/defaults has no config',
+                    new File(buildDir, 'build/wlp/usr/servers/defaultServer/configDropins/defaults').list().size() == 1)
+            File configDropinsDir=new File(buildDir, 'build/wlp/usr/servers/defaultServer/configDropins/defaults')
+            File configDropinsFile=new File(configDropinsDir,configDropinsDir.list().getAt(0))
+            try (FileInputStream input = new FileInputStream(configDropinsFile)) {
+                // get configDropins XML Document
+                DocumentBuilderFactory inputBuilderFactory = DocumentBuilderFactory.newInstance();
+                inputBuilderFactory.setIgnoringComments(true);
+                inputBuilderFactory.setCoalescing(true);
+                inputBuilderFactory.setIgnoringElementContentWhitespace(true);
+                inputBuilderFactory.setValidating(false);
+                inputBuilderFactory.setFeature("http://apache.org/xml/features/nonvalidating/load-dtd-grammar", false);
+                inputBuilderFactory.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
+                DocumentBuilder inputBuilder = inputBuilderFactory.newDocumentBuilder();
+                Document inputDoc=inputBuilder.parse(input);
+
+                // parse configDropins XML Document
+                XPath xPath = XPathFactory.newInstance().newXPath();
+                String expression = "/server/springBootApplication";
+                NodeList nodes = (NodeList) xPath.compile(expression).evaluate(inputDoc, XPathConstants.NODESET);
+                Assert.assertTrue("Number of <springBootApplication/> element ==>", nodes.getLength()>0);
+
+                Node node = nodes.item(0);
+                Element element = (Element)node;
+                Assert.assertEquals("Value of the 1st <springBootApplication/> ==>"+element.getAttribute("location"), "thin-${testName.getMethodName()}-1.0-SNAPSHOT.jar".toString(), element.getAttribute("location"));
+            }
+            Assert.assertTrue('no app in apps folder',
+                    new File(buildDir, "build/wlp/usr/servers/defaultServer/apps/thin-${testName.getMethodName()}-1.0-SNAPSHOT.jar").exists() )
+        } catch (Exception e) {
+            throw new AssertionError ("Fail on task deploy.", e)
+        }
+    }
+
+    @Test
+    public void test_spring_boot_classifier_apps_40() {
+        try {
+            runTasks(buildDir, 'deploy', 'libertyStart')
+
+            String webPage = new URL("http://localhost:9080").getText()
+            Assert.assertEquals("Did not get expected http response.","Hello!", webPage)
+            Assert.assertTrue('defaultServer/dropins has app deployed',
+                    new File(buildDir, 'build/wlp/usr/servers/defaultServer/dropins').list().size() == 0)
+            Assert.assertTrue('no app in apps folder',
+                    new File(buildDir, "build/wlp/usr/servers/defaultServer/apps/thin-${testName.getMethodName()}-1.0-SNAPSHOT-test.jar").exists() )
+        } catch (Exception e) {
+            throw new AssertionError ("Fail on task deploy.", e)
+        }
+    }
+
+    @Test
+    public void test_spring_boot_classifier_apps_40_no_feature() {
+        try {
+            runTasks(buildDir, 'deploy')
+
+            Assert.assertTrue('defaultServer/dropins has app deployed',
+                    new File(buildDir, 'build/wlp/usr/servers/defaultServer/dropins').list().size() == 0)
+            Assert.assertTrue('no app in apps folder',
+                    new File(buildDir, "build/wlp/usr/servers/defaultServer/apps/thin-${testName.getMethodName()}-1.0-SNAPSHOT-test.jar").exists() )
+        } catch (Exception e) {
+            throw new AssertionError ("Fail on task deploy.", e)
+        }
+    }
+
+    @Test
+    public void test_spring_boot_war_apps_40() {
+        try {
+            runTasks(buildDir, 'deploy', 'libertyStart')
+
+            String webPage = new URL("http://localhost:9080").getText()
+            Assert.assertEquals("Did not get expected http response.","Hello!", webPage)
+            Assert.assertTrue('defaultServer/dropins has app deployed',
+                    new File(buildDir, 'build/wlp/usr/servers/defaultServer/dropins').list().size() == 0)
+            Assert.assertTrue('no app in apps folder',
+                    new File(buildDir, "build/wlp/usr/servers/defaultServer/apps/thin-${testName.getMethodName()}-1.0-SNAPSHOT.war").exists() )
+        } catch (Exception e) {
+            throw new AssertionError ("Fail on task deploy.", e)
+        }
+    }
+
+    @Test
+    public void test_spring_boot_war_classifier_apps_40() {
+        try {
+            runTasks(buildDir, 'deploy', 'libertyStart')
+
+            String webPage = new URL("http://localhost:9080").getText()
+            Assert.assertEquals("Did not get expected http response.","Hello!", webPage)
+            Assert.assertTrue('defaultServer/dropins has app deployed',
+                    new File(buildDir, 'build/wlp/usr/servers/defaultServer/dropins').list().size() == 0)
+            Assert.assertTrue('no app in apps folder',
+                    new File(buildDir, "build/wlp/usr/servers/defaultServer/apps/thin-${testName.getMethodName()}-1.0-SNAPSHOT-test.war").exists() )
+        } catch (Exception e) {
+            throw new AssertionError ("Fail on task deploy.", e)
+        }
+    }
+
+    @Test
+    public void test_spring_boot_dropins_40() {
+        try {
+            runTasks(buildDir, 'deploy', 'libertyStart')
+            String webPage = new URL("http://localhost:9080").getText()
+            Assert.assertEquals("Did not get expected http response.","Hello!", webPage)
+            Assert.assertTrue('defaultServer/dropins/spring has no app',
+                    new File(buildDir, "build/wlp/usr/servers/defaultServer/dropins/spring/thin-${testName.getMethodName()}-1.0-SNAPSHOT.jar").exists())
+            Assert.assertTrue('apps folder should be empty',
+                    new File(buildDir, "build/wlp/usr/servers/defaultServer/apps").list().size() == 0 )
+        } catch (Exception e) {
+            throw new AssertionError ("Fail on task deploy.", e)
+        }
+    }
+
+    @Test
+    public void test_spring_boot_plugins_dsl_apps_40() {
+        try {
+            runTasks(buildDir, 'deploy', 'libertyStart')
+
+            String webPage = new URL("http://localhost:9080").getText()
+            Assert.assertEquals("Did not get expected http response.","Hello!", webPage)
+            Assert.assertTrue('defaultServer/dropins has app deployed',
+                    new File(buildDir, 'build/wlp/usr/servers/defaultServer/dropins').list().size() == 0)
+            Assert.assertTrue('no app in apps folder',
+                    new File(buildDir, "build/wlp/usr/servers/defaultServer/apps/thin-${testName.getMethodName()}-1.0-SNAPSHOT.jar").exists() )
+        } catch (Exception e) {
+            throw new AssertionError ("Fail on task deploy.", e)
+        }
+    }
+
+    @Test
+    public void test_spring_boot_with_springbootapplication_apps_40() {
+        try {
+            runTasks(buildDir, 'deploy', 'libertyStart')
+
+            String webPage = new URL("http://localhost:9080").getText()
+            Assert.assertEquals("Did not get expected http response.","Hello!", webPage)
+            Assert.assertTrue('defaultServer/dropins has app deployed',
+                    new File(buildDir, 'build/wlp/usr/servers/defaultServer/dropins').list().size() == 0)
+            Assert.assertTrue('generated thin app name not same as specified in server.xml <SpingBootApplication/> node',
+                    new File(buildDir, "build/wlp/usr/servers/defaultServer/apps/${testName.getMethodName()}-1.0-SNAPSHOT.jar").exists() )
+        } catch (Exception e) {
+            throw new AssertionError ("Fail on task deploy.", e)
+        }
+    }
+
+    @Test
+    public void test_spring_boot_with_springbootapplication_nodes_apps_40() {
+        try {
+            BuildResult result = runTasksFailResult(buildDir, 'deploy', 'libertyStart')
+            String output = result.getOutput()
+            assertTrue(output.contains("Found multiple springBootApplication elements specified in the server configuration"))
+        } catch (Exception e) {
+            throw new AssertionError ("Fail on task deploy.", e)
+        }
+    }
+
+    @Test
+    public void test_spring_boot_with_springbootapplication_nodes_apps_include_40() {
+        try {
+            BuildResult result = runTasksFailResult(buildDir, 'deploy', 'libertyStart')
+            String output = result.getOutput()
+            assertTrue(output.contains("Found multiple springBootApplication elements specified in the server configuration in files"))
+            assertTrue(output.contains("included_server.xml"))
+        } catch (Exception e) {
+            throw new AssertionError ("Fail on task deploy.", e)
+        }
+    }
+
+    @Test
+    public void test_spring_boot_apps_40_unsupported_liberty_version() {
+        try {
+            BuildResult result = runTasksFailResult(buildDir, 'deploy', '-PtestRuntimeVersion=25.0.0.12')
+            String output = result.getOutput()
+            assertTrue(output.contains("Spring Boot 4.0.5 applications require Liberty runtime version 26.0.0.5 or later. The configured Liberty runtime version is 25.0.0.12."))
+        } catch (Exception e) {
+            throw new AssertionError ("Fail on task deploy.", e)
+        }
+    }
+
+
+}
